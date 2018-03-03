15b6bfaff986e702eba066d56e07cb096776f351
Nico Verzijp <nico.verzijp@nokia.com>
2018-03-03 14:14:11 +0100

    add frontend

--- src/app.ts
+++ src/app.ts
@@ -6,26 +6,10 @@ class App {
   constructor () {
     this.app = express()
     this.config()
-    this.routes()
   }
 
   private config (): void {
-    // only serve static assets in production
-    if (process.env.NODE_ENV === 'production') {
-      this.app.use(express.static('public'))
-    }
-  }
-
-  private routes (): void {
-    /*
-    const router = express.Router()
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!'
-      })
-    })
-    this.app.use('/', router)
-    */
+    this.app.use(express.static('./public'))
   }
 }
 
