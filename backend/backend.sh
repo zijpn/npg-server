78141510fe1043f14e8545f0ae9b06ede514f8c9
Nico Verzijp <nico.verzijp@nokia.com>
2018-03-08 14:41:26 +0100

    add backend

---
+++ backend/backend.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# https://blog.codeship.com/using-swarm-with-calico-on-docker-machine/
+# https://blog.scottlowe.org/2015/08/04/using-vagrant-docker-machine-together/
+
+case "$1" in
+  "create")
+    vagrant plugin install vagrant-vbguest
+    vagrant up
+    docker-machine create -d generic --generic-ssh-user vagrant --generic-ssh-key ~/.vagrant.d/insecure_private_key --generic-ip-address 192.168.99.101 npg-01
+    docker-machine create -d generic --generic-ssh-user vagrant --generic-ssh-key ~/.vagrant.d/insecure_private_key --generic-ip-address 192.168.99.102 npg-02
+    ;;
+
+  "destroy")
+    docker-machine rm -y npg-01
+    docker-machine rm -y npg-02
+    vagrant destroy -f
+    ;;
+
+  "status")
+    docker-machine ls
+    ;;
+
+  "test")
+    echo "npg-01"
+    echo "------"
+    docker-machine ssh npg-01 etcd --version
+    docker-machine ssh npg-01 calicoctl --version
+    docker-machine ssh npg-01 sudo calicoctl node checksystem
+    docker-machine ssh npg-01 sudo calicoctl node status
+    echo ""
+    echo "npg-02"
+    echo "------"
+    docker-machine ssh npg-02 calicoctl --version
+    docker-machine ssh npg-02 sudo calicoctl node checksystem
+    docker-machine ssh npg-02 sudo calicoctl node status
+    ;;
+  
+  *)
+    echo "usage: $0 [ create | destroy | status | test]"
+    ;;
+esac
